b102da8fb03f3c9a6d26acbb9dee66b7f412203a
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-06 11:02:32 -0700

    Move files from test to root folder

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function to test printf
+ * Return: Always 0
+ */
+
+int main(void)
+{
+/*	printf("%d", _putchar_s("ssssssss"));*/
+	printf("%d", _printf("a%sja%%ha\nvico%c%", NULL, 't'));
+	return (0);
+}
